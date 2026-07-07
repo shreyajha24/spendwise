13c370ea631092e9b1a4cf8e4c030edca83f7e90
Shreya Jha <shreyajha2430@gmail.com>
2026-07-07 17:32:45 +0530

    implemented exception section and modified service accordingly

--- src/main/java/com/shreya/spendwise/exception/ErrorResponse.java
+++ src/main/java/com/shreya/spendwise/exception/ErrorResponse.java
@@ -1,4 +1,17 @@
 package com.shreya.spendwise.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+@Getter
+@AllArgsConstructor
 public class ErrorResponse {
+
+ private int status;
+
+ private String message;
+
+ private LocalDateTime timestamp;
+
 }
--- src/main/java/com/shreya/spendwise/exception/GlobalExceptionHandler.java
+++ src/main/java/com/shreya/spendwise/exception/GlobalExceptionHandler.java
@@ -1,4 +1,23 @@
 package com.shreya.spendwise.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ExpenseNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleExpenseNotFound(
+            ExpenseNotFoundException ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 }
